fcece9de2ebc3062fca1f8eb79cda1710d5e2d71
Oleg <malchishka08@ukr.net>
2022-11-26 22:33:21 +0200

    Added my HomeWork

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const firstNum = Number(prompt("Enter first number:"));
+const secondNum = Number(prompt("Enter second number:"));
+alert(firstNum + secondNum);
+alert(firstNum - secondNum);
+alert(firstNum * secondNum);
+alert(firstNum / secondNum);
