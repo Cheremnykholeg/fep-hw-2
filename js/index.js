fca66052653ad5459e7491241500faadfb94d972
Oleg <malchishka08@ukr.net>
2022-11-26 23:37:03 +0200

    Update

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const firstNum = Number(prompt("Enter first number:"));
-const secondNum = Number(prompt("Enter second number:"));
-alert(firstNum + secondNum);
-alert(firstNum - secondNum);
-alert(firstNum * secondNum);
-alert(firstNum / secondNum);
